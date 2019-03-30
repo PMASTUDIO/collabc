da18819ec0c40bd210acff5e019db639b4eb6ca5
Pedro Ganme <pmastudionet@gmail.com>
2019-03-30 10:56:17 -0300

    Top projects layout added!

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,11 +8,29 @@ export default class HomePage extends React.Component {
     render() {
         return (
             [<header key="header">
-                <img className="responsiveLogo" src="./images/Logo.png"></img>
+                <img className="img-fluid" src="./images/Logo.png"></img>
             </header>,
             <main key="main">
-                <div className="mid-content">
-                    p
+                <div className="mid-content container text-center">
+                    <h1 className="text-uppercase">Top Projects</h1>
+                    <div className="row">
+                        <div className="col-sm-3 top-item">
+                            <h2>Game Engine</h2>
+                            <h3>The Cherno</h3>
+                            <p>Description</p>
+                        </div>
+                        <div className="col-sm-3 top-item">
+                            <h2>Game Engine</h2>
+                            <h3>The Cherno</h3>
+                            <p>Description</p>
+                        </div>
+                        <div className="col-sm-3 top-item">
+                            <h2>Game Engine</h2>
+                            <h3>The Cherno</h3>
+                            <p>Description</p>
+                        </div>
+                        
+                    </div>
                 </div>
             </main>]
         );
