770724025c257a6a8e03ca5faa4526cfb7a370d7
PMASTUDIO <pma@pmastudio.net>
2019-04-12 12:02:45 -0700

    Front end small chages, and bootstrap optimization, and modaladded

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+import $ from 'jQuery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../dist/css/main.css';
+
 ReactDOM.render(
     <App />,
     document.querySelector('.app')
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,22 @@ export default class HomePage extends React.Component {
     constructor(){
         super();
         this.state = {
-            topProjects: []
+            topProjects: [{
+                    name: "Hello Jonathan",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate.",
+                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                },
+                {
+                    name: "Hello Filipe",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
+                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                },
+                {
+                    name: "Hello Pedro",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
+                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                }
+            ]
         }
     }
 
@@ -15,24 +30,45 @@ export default class HomePage extends React.Component {
             </header>,
             <main key="main">
                 <div className="container">
-                    <form className="navbar-form" role="search">
-                        <div className="input-group add-on">
-                            <input className="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text"/>
-                            <div className="input-group-btn">
-                                <button className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i></button>
-                            </div>
+                    <div className="input-group add-on">
+                        <input className="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text"/>
+                        <div className="input-group-btn">
+                            <button className="btn btn-light" type="submit"><i className="fas fa-search"></i></button>
                         </div>
-                    </form>
+                    </div>
                 </div>
                 
                 <div className="mid-content container text-center">
                     <h1 className="text-uppercase">Top Projects</h1>
                     <div className="row">
-                    {this.state.topProjects.map((project) => (
-                        <div className="col-sm-3 top-item">
+                    {this.state.topProjects.map((project, i) => (
+                        [<div key={i} className="col-sm-3 top-item">
                             <h2>{project.name}</h2>
-                            <p>{project.description}</p>
+                            <p >{project.description}</p>
+                            <button type="button" data-toggle="modal" data-target={"#modalMore" + i} className="btn btn-primary">More</button>
+                        </div>,
+                        
+                        <div key={"id" + i} className="modal fade" id={"modalMore" + i} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div className="modal-dialog" role="document">
+                                <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLongTitle">{project.name}</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    {project.descriptionLong}
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary">Save changes</button>
+                                </div>
+                                </div>
+                            </div>
                         </div>
+                        ]
+                        
                     ))}
                     </div>
                 </div>
