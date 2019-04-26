db3b3c81a4015c9efc472535bb60aec9bf27afde
PMASTUDIO <pma@pmastudio.net>
2019-04-26 11:21:13 -0700

    State home page optimized

---
+++ src/category.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+export default class Category extends React.Component {
+
+    render() {
+        return (
+            [<h1 key="title" className="text-uppercase">{this.props.category.name}</h1>,
+            <div key="projects" className="row">
+                {this.props.category.projects.map((project, i) => (
+                    [<div key={i} className="col-sm-3 top-item">
+                    <h2>{project.name}</h2>
+                    <p >{project.description}</p>
+                    <button type="button" data-toggle="modal" data-target={"#modalMore" + i} className="btn btn-primary">More</button>
+                </div>,
+                
+                <div key={"id" + i} className="modal fade" id={"modalMore" + i} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">{project.name}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {project.descriptionLong}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn-null"><i className="fas fa-star"></i></button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                ]
+                ))}
+            </div>]
+        );
+    }
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import Category from '../category.jsx'
 
 export default class HomePage extends React.Component {
     constructor(){
         super();
         this.state = {
-            topProjects: [{
-                    name: "Hello Jonathan",
-                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate.",
-                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
-                },
+            categories: [
                 {
-                    name: "Hello Filipe",
-                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
-                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
-                },
-                {
-                    name: "Hello Pedro",
-                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
-                    descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                    name: "Top Projects",
+                    projects: [{
+                        name: "Hello Jonathan",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate.",
+                        descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                    },
+                    {
+                        name: "Hello Filipe",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
+                        descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                    },
+                    {
+                        name: "Hello Pedro",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt animi expedita deserunt. Eos numquam nulla, non deserunt optio sequi commodi odit impedit corporis cupiditate. Quo deleniti numquam praesentium soluta.",
+                        descriptionLong: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam hic deserunt consequatur atque sapiente ipsam nemo, dolores rem? At, non sed minima nobis doloribus rerum perspiciatis maxime officia officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic, iste accusamus praesentium deleniti rerum obcaecati explicabo cupiditate at, nisi amet deserunt officiis molestias nostrum atque vero! Enim, id expedita."
+                    }]
                 }
             ]
         }
@@ -42,38 +47,10 @@ export default class HomePage extends React.Component {
                 </div>
                 
                 <div className="mid-content container text-center">
-                    <h1 className="text-uppercase">Top Projects</h1>
-                    <div className="row">
-                    {this.state.topProjects.map((project, i) => (
-                        [<div key={i} className="col-sm-3 top-item">
-                            <h2>{project.name}</h2>
-                            <p >{project.description}</p>
-                            <button type="button" data-toggle="modal" data-target={"#modalMore" + i} className="btn btn-primary">More</button>
-                        </div>,
-                        
-                        <div key={"id" + i} className="modal fade" id={"modalMore" + i} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                            <div className="modal-dialog" role="document">
-                                <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="exampleModalLongTitle">{project.name}</h5>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body">
-                                    {project.descriptionLong}
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" className="btn-null"><i className="fas fa-star"></i></button>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                        ]
-                        
+                    {this.state.categories.map((categoryMap, j) => (
+                        <Category category={categoryMap} />
                     ))}
-                    </div>
+                    
                 </div>
             </main>]
         );
