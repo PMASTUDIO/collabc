aed3c48e92cd786e5a1e6ec2dfc3879964ab58f4
PMASTUDIO <pma@pmastudio.net>
2019-05-03 12:01:28 -0700

    Optimizations Webpack added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-import $ from 'jQuery';
+//import $ from 'jQuery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,48 @@
+const TerserJS = require('terser-webpack-plugin')
+const OptimizeCssAssets = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const imageminOptipng = require('imagemin-optipng')
+const imageminJpegtran = require('imagemin-jpegtran')
+
 module.exports = {
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/html/index.html'
+        }),
+        new CopyPlugin([{
+            from: 'dist/images/'
+        }]),
+        new ImageminPlugin({
+            imageminOptions: {
+                plugins: [
+                    imageminJpegtran({
+                        progressive: true
+                    }),
+                    imageminOptipng({
+                        optimizationLevel: 1
+                    }),
+                ]
+            }
+        }),
+    ],
+    devtool: process.env.NODE_ENV === 'development'? "source-map" : "",
+    optimization: {
+        minimizer: [new TerserJS({}), new OptimizeCssAssets({})],
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+
     module: {
         rules: [
             {
@@ -10,7 +54,15 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [ "style-loader", "css-loader" ]
+                use: [ {
+                    loader: MiniCssExtractPlugin.loader,
+                    options:{
+                        publicPath: 'dist',
+                        hmr: process.env.NODE_ENV === 'development',
+                        reloadAll: true,
+                    },
+                }, 
+                "css-loader" ]
             }
         ]
     },
